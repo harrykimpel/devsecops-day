79dae701257b7196f357e4cd28480607a680432a
Harry Kimpel <hkimpel@newrelic.com>
2021-03-02 20:06:35 +0100

    Adding demo 2

---
+++ second-religion/nerdlets/second-religion-nerdlet/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class SecondReligionNerdletNerdlet extends React.Component {
+    render() {
+        return <><h1>Hallo, DevSecOps Day Nerdlet!</h1>
+            <p><a href="https://sre.google/sre-book/service-level-objectives/" target="_new">Google SRE SLOs</a></p></>;
+    }
+}
